7f01d75c59af0ceb87db5c937b0cedbe158c47c9
mansour naslcheraghi <imans@MansBookPro.local>
2020-08-03 04:31:05 -0400

    Initial Commit

---
+++ script.py
@@ -0,0 +1,24 @@
+import sys
+import math
+import os
+
+# import requests
+
+
+# print (sys.version)
+name = input("Your name? ")
+print("Hello,", name)
+# print(sys.executable)
+
+
+# def greet(who_to_greet):
+#   greeting = 'Hello, {}' .format(who_to_greet)
+#  return greeting
+
+
+# r = requests.get('https://google.com')
+# print(r.status_code)
+
+
+# print(greet('World'))
+# print(greet('Corey'))
