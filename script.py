dd0b8956729db56752b9fdc2d38aa9ebb5f9aea0
mansour naslcheraghi <imans@MansBookPro.local>
2020-08-05 02:53:22 -0400

    I tried to fix the for-loop

--- script.py
+++ script.py
@@ -1,15 +1,23 @@
 import sys
 import math
+import numpy as np
 import os
+import array as arr
 
-# import requests
+M = list(range(1, 1000))
+z = list(range(1, 1000))
+
+zipf_exp = 0.6
+fre = np.empty((1, M))
+
+for file_index in M:
+    numen = 1 / file_index ** zipf_exp
+    denum = sum(1 / (z ** zipf_exp))
+    fre[file_index - 1] = numen / denum
+
+print(fre)
 
 
-# print (sys.version)
-name = input("Your name? ")
-print("Hello,", name)
-# print(sys.executable)
+# frequency_i = (1/(file_index^zipf_exp))/sum( 1/(z^(zipf_exp)))
 
-
-# print(greet('World'))
-# print(greet('Corey'))
+# print(sum(1 / (z ** (zipf_exp))))
